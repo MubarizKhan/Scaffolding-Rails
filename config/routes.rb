8cd1808c5863471458d4da313c05f3c656506108
MubarizKhan <p176007@nu.edu.pk>
2022-09-06 13:38:33 +0500

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :post
   root 'posts#index'
+  # root :to => "home#index"
   # resources :
 end
